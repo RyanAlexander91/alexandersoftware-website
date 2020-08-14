df2fce3ea5b711adb810da370bdff81cd4ab7f91
Ryan Alexander <10535959+RyanAlexander91@users.noreply.github.com>
2020-08-14 01:39:54 +0100

    Added Index Page

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# alexandersoftware-website
-Portfolio Website built from scratch using HTML, CSS and JavaScript
+# AlexanderSoftware Website
+My Portfolio website built from scratch using HTML5, CSS and JavaScript.
+
+https://www.alexandersoftware.com
+
+## Table of Contents
+---
+
+1. [Project Aims:](#project-aims)
+    * [Project Targets:](#project-targets)
+2. [Current Issues:](#current-issues)
+3. [Lessons Learned: (Coming Soon)](#lessons-learned)
+4. [Future Developments: (Coming Soon)](#future-developments)
+
+## Project Aims:
+
+The main goal when deciding to build my portfolio website, was to improve my web development skills. As such I decided I not to use any frontend frameworks or libraries to aid me.
+
+### Project targets:
+
+* **Responsive** - The website had to work regardless of device used.
+* **Accessible** - The website had to be accssible for all users including those who use screen readers. 
+* **Browser Support** - Website functionally should work on all *currently supported* browsers. 
+
+## Current Issues:
+
+The current issues can be found on [Project Issues](https://github.com/RyanAlexander91/Portfolio-Website/issues). If you find an issue or have any feedback, please let me know by posting an issue and I'll get back to you as soon as I can.
+
+## Lessons Learned: (Coming Soon)
+
+As the project is still under development, this section will be added later. 
+
+## Future Developments: (Coming Soon)
+
+As the project is still under development, this section will be added later.
---
+++ js/site.js
@@ -0,0 +1,89 @@
+/**
+ * Ryan Alexander Website - Last Updated: 04/02/2020
+ * 
+ * This document uses strict mode JavaScript. 
+ * 
+ * I have tried to make my code as clean as possible, however I am still learning Javascript so there will mistakes. 
+ * 
+ * If you spot any errors or there are any code improvements I can make, please let me know on the project's issue page.
+ * 
+ */
+
+// Use strict mode JavaScript
+"use strict";
+
+/** Application is encapsulated in a IIFE (Immediately Invoked Function Expression)
+ * this is to stop conflicts in the global namespace 
+ * */
+(function () {
+
+    // Handle Navigation Icon Clicks
+    const BUTTON_HAMBURGER = document.getElementById("button-hamburger");
+    BUTTON_HAMBURGER.addEventListener("click", toggleMobileMenu);
+
+    /**
+     * This function runs whenever the hamburger button (Anchor Tag) is clicked
+     */
+    function toggleMobileMenu() {
+
+        // Add reference to menu buttons
+        const HEADER = document.getElementById("website-header");
+        const ICON_HAMBURGER = document.getElementById("icon-hamburger");
+        const NAVIGATION_SECTION = document.getElementById("navigation-section");
+        const NAVIGATION_LINKS = document.getElementsByClassName("navigation-link");
+        const MENU_SOCIAL_SECTION = document.getElementById("menu-social-section");
+        const HERO_CONTENT = document.getElementById("hero-content");
+        const HERO_BUTTON = document.getElementById("hero-button");
+
+
+        // Change Menu Icon
+        if (ICON_HAMBURGER.classList.contains("fa-bars")) {
+            // Enable Mobile Menu
+            openMobileMenu();
+        }
+        else {
+            // Disable Mobile Menu
+            closeMobileMenu();
+        }
+
+        /**
+         * Adds the "mobile-menu-enabled" class to display mobile menu through CSS
+         */
+        function openMobileMenu() {
+            BUTTON_HAMBURGER.classList.add("mobile-menu-enabled");
+            ICON_HAMBURGER.classList.remove("fa-bars");
+            ICON_HAMBURGER.classList.add("fa-times");
+            HEADER.classList.add("mobile-menu-enabled");
+            NAVIGATION_SECTION.classList.add("mobile-menu-enabled");
+            MENU_SOCIAL_SECTION.classList.add("mobile-menu-enabled");
+            HERO_CONTENT.classList.add("mobile-menu-enabled");
+            HERO_BUTTON.classList.add("mobile-menu-enabled");
+
+            // Add Event Listener to each Navigation Link to close 
+            for (let index = 0; index < NAVIGATION_LINKS.length; index++) {
+                NAVIGATION_LINKS[index].classList.add("mobile-menu-enabled");
+                NAVIGATION_LINKS[index].addEventListener("click", closeMobileMenu);
+            }
+        }
+
+        /**
+         * Removes the "mobile-menu-enabled" class to hide mobile menu through CSS
+         */
+        function closeMobileMenu() {
+            BUTTON_HAMBURGER.classList.remove("mobile-menu-enabled");
+            ICON_HAMBURGER.classList.remove("fa-times");
+            ICON_HAMBURGER.classList.add("fa-bars");
+            HEADER.classList.remove("mobile-menu-enabled");
+            NAVIGATION_SECTION.classList.remove("mobile-menu-enabled");
+            MENU_SOCIAL_SECTION.classList.remove("mobile-menu-enabled");
+            HERO_CONTENT.classList.remove("mobile-menu-enabled");
+            HERO_BUTTON.classList.remove("mobile-menu-enabled");
+
+            // Remove Event Listeners from each Navigation Link
+            for (let index = 0; index < NAVIGATION_LINKS.length; index++) {
+                NAVIGATION_LINKS[index].classList.remove("mobile-menu-enabled");
+                NAVIGATION_LINKS[index].removeEventListener("click", closeMobileMenu);
+            }
+        }
+    }
+})();
